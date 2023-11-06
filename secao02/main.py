f25cc23c6103359e67620ff9448f514c0d5253f8
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2023-11-06 16:48:40 -0300

    Adicionando config de execução

--- secao02/main.py
+++ secao02/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get('/')
 async def raiz():
     return {'msg': 'Curso de FastAPI'}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info', reload=True)
