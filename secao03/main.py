3ade928fff89e47d50fbd8dd65bf19ed35b8afbf
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2023-11-07 14:43:07 -0300

    Adicionando consulta de único curso

--- secao03/main.py
+++ secao03/main.py
@@ -19,6 +19,13 @@ cursos = {
 async def get_cursos():
     return cursos
 
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
+    curso.update({'id': curso_id})
+
+    return curso
+
 if __name__ == "__main__":
     import uvicorn
 
