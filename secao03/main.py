e90c3f1e6e552a8119485430bb546e14af166a39
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2024-03-01 10:20:23 -0300

    Implementando documentacao em todas as funcoes de cursos

--- secao03/main.py
+++ secao03/main.py
@@ -1,8 +1,9 @@
+import enum
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi import Path, Query, Header, Depends
-from typing import Any, Optional
 from models import Curso, cursos
+from typing import Any, Optional, Dict, List
 from time import sleep
 
 def fake_db():
@@ -31,42 +32,64 @@ async def get_calculadora(
     print(f'X_GEEK: {x_geek}')
     return {'resultado': soma}
 
-@app.get('/cursos')
+@app.get('/cursos',
+         description="Retorna todos os Cursos ou uma Lista Vazia",
+         summary="Retorna todos os cursos",
+         response_model=List[Curso],
+         response_description='Cursos encontrados com sucesso')
 async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
-@app.get('/cursos/{curso_id}')
+@app.get('/cursos/{curso_id}',
+         description="Retorna o Curso Consultado ou Erro",
+         summary="Retorna um curso",
+         response_model=Curso,
+         response_description="Curso Encontrado com sucesso")
 async def get_curso(curso_id: int = Path(title='ID do Curso', description='Deve Ser entre 1 e 2', gt=0, lt=3), db: Any = Depends(fake_db)):
     try:
-        curso = cursos[curso_id]
-        curso.update({'id': curso_id})
-        return curso
+        for index, curso in enumerate(cursos):
+            if curso.id == curso_id:
+                return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Cruso não encontrado')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Cruso não encontrado')
     
-@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+@app.post('/cursos',
+          status_code=status.HTTP_201_CREATED,
+          description="Cadastra ou Sobrescreve um Curso",
+          summary="Cadastra um curso",
+          response_model=Curso,
+          response_description="Curso Cadastrado")
 async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
-    next_id = len(cursos) + 1
-    del curso.id
-    cursos[next_id] = curso
+    for i, item in enumerate(cursos):
+        if item.id == curso.id:
+            cursos[i] = curso
+            return curso
+    cursos.append(curso)
     return curso
 
-@app.put('/cursos/{curso_id}')
+@app.put('/cursos/{curso_id}',
+         description="Atualiza um Curso, retorna erro caso não encontra",
+         summary="Atualiza Um Curso",
+         response_model=Curso,
+         response_description="Curso Atualizado")
 async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
-    if curso_id not in cursos:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não Existe um curso com ID {curso_id}.')
+    for i, item in enumerate(cursos):
+        if item.id == curso_id:
+            cursos[i] = curso
+            return curso
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não Existe um curso com ID {curso_id}.')
 
-    del curso.id
-    cursos[curso_id] = curso
-    return curso
-
-@app.delete('/cursos/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/cursos/{curso_id}',
+            status_code=status.HTTP_204_NO_CONTENT,
+            description="Deleta um curso, retorna erro caso não encontra",
+            summary="Deleta um curso")
 async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
-    if curso_id not in cursos:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não Existe um curso com ID {curso_id}.')
-
-    del cursos[curso_id]
-    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content='')
+    for i, item in enumerate(cursos):
+        if item.id == curso_id:
+            cursos.remove(item)
+            return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content='')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não Existe um curso com ID {curso_id}.')
     
 
 if __name__ == "__main__":
