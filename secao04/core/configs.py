586b3a48d552807114e88df0dec624db87a1a998
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2024-03-01 11:55:42 -0300

    Declaranto tipo Any para DBBaseModel

--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pydantic_settings import BaseSettings
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,7 +9,7 @@ class Settings(BaseSettings):
     """
     API_V1_STR: str = "/api/v1"
     DB_URL: str = "postgresql+asyncpg://curso:curso@localhost:5432/cursofastapi"
-    DBBaseModel = declarative_base()
+    DBBaseModel: Any = declarative_base()
 
     class Config:
         case_sensitive = True
