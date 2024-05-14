536758b183cb8a4ef084ebee21c76c4f694973b9
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2024-05-14 10:41:49 -0300

    Definindo tipo de DBBaseModel

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -1,10 +1,11 @@
+from typing import ClassVar
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     DB_URL: str = "postgresql+asyncpg://curso:curso@localhost:5432/cursofastapi"
-    DBBaseModel = declarative_base()
+    DBBaseModel: ClassVar = declarative_base()
 
     JWT_SECRET: str = "0dpdlL2l9oZCADJAKsS94Veq2748mgr_xHUHwTekqsk"
     """
