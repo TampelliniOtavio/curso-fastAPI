3971222896cdf2f60f8e3e6df4475279ed323940
Otavio Dantas <otavio.dantas@federalconsig.com.br>
2024-05-14 09:28:55 -0300

    Criando Arquivo de Config

---
+++ secao06/core/configs.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    DB_URL: str = "postgresql+asyncpg://curso:curso@localhost:5432/cursofastapi"
+    DBBaseModel = declarative_base()
+
+    JWT_SECRET: str = "0dpdlL2l9oZCADJAKsS94Veq2748mgr_xHUHwTekqsk"
+    """
+    import secrets
+
+    token: str = secrets.token_urlsafe(32)
+    """
+    ALGORITHM: str = "HS256"
+
+    # 60 Minutos * 24 Horas * 7 Dias = 1 Semana
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    class Config:
+        case_sensitive = True
+
+settings: Settings = Settings()
